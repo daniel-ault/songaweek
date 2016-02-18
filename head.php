a03aa1b69527565f438a12839c3a5d139245b515
Daniel Ault <dault@memphis.edu>
2016-02-18 02:05:32 -0600

    copied style and footer from songaweek2016.com

--- head.php
+++ head.php
@@ -14,5 +14,73 @@
 	<link href="css/style.css" rel="stylesheet">
 
 	<?php include 'php_utils.php'?>
+	
+	<link rel="stylesheet" href="//55b558c7-site.gandi.ws/_css/site_69711_1_11.css">
+
+
+
+    
+            <style
+            class="js-webfont"
+            data-woff-cache="//55b558c7-resources.gandi.ws/3e64db8/seven/fonts/lato.woff.json"
+            data-ttf-cache="//55b558c7-resources.gandi.ws/3e64db8/seven/fonts/lato.ttf.json"
+            data-fallback-stylesheet="https://fonts.googleapis.com/css?family=Lato&amp;subset=latin"
+            data-name="lato"
+        ></style>
+    <script>
+(function () {
+    var fontCollections,
+        fontsToCache,
+        fallback = 1;
+
+    function loadFontsAsynchronously() {
+        if (!fallback) {
+            return;
+        }
+
+        for (var i = 0, j = fontCollections.length; i < j; ++i) {
+            var link = document.createElement('link'),
+                head = document.getElementsByTagName('head')[0];
+            link.rel = 'stylesheet';
+            link.href = fontCollections[i].getAttribute('data-fallback-stylesheet');
+            head.appendChild(link);
+        }
+    }
+
+    function loadFontsFromStorage() {
+        var localKey = 'published',
+            format = window.localStorage.getItem('bk.webfonts.format'),
+            collection,
+            cachedFont;
+
+        if (!format) {
+            return;
+        }
+
+        for (var i = 0, j = fontCollections.length; i < j; ++i) {
+            collection = fontCollections[i];
+            cacheKey = 'bk.webfonts.' + localKey + '.' + format + '.' + collection.getAttribute('data-name');
+            cachedFont = window.localStorage.getItem(cacheKey);
+            if (!cachedFont) {
+                continue;
+            }
+            collection.innerHTML = cachedFont;
+            collection.setAttribute('data-cache-full', 'data-cache-full');
+        }
+    }
+
+    if (!'querySelectorAll' in window) {
+        return;
+    }
+
+    fontCollections = document.head.querySelectorAll('.js-webfont');
+    if (!'localStorage' in window || !'addEventListener' in window) {
+        loadFontsAsynchronously();
+        return;
+    }
+
+    loadFontsFromStorage();
+}());
+</script>
 
 </head>
--- index.php
+++ index.php
@@ -32,6 +32,111 @@
 	</div>
 </div>
 
+
+<footer class="pageFooter">
+    <div class="wrapper clearfix">
+        <div class="responsivecolumns columns-two-50-50">
+           <div class="column columns-two-left">
+                <div id="page-zones__template-widgets__content-footercolumn1content1" class="widget content " data-widget-type="content" data-uniqueid="page-zones__template-widgets__content-footercolumn1content1">
+      <div class="bk-content-text js-text-content wysihtml-editor-content">
+                <h3>Subscribe:</h3>
+                </div>
+
+    
+</div>
+
+
+                <div id="page-zones__template-widgets__profile-footercolumn2profileaddress" class="widget profile " data-widget-type="profile" data-uniqueid="page-zones__template-widgets__profile-footercolumn2profileaddress">
+  
+
+
+
+
+
+
+
+
+
+
+
+<form action="" method="post" class="js-form">
+  <fieldset>
+    <div class="form-group"> 
+      <label for="profileform-email">Your email:</label>
+      <input type="email" class="email js-email" placeholder="Type your email" id="profileform-email" name="profileform-email">
+
+      <button class="btn btnText  " type="submit" >Sign me up!</button>
+    </div>
+  </fieldset>
+</form>
+
+
+
+</div>
+
+
+           </div>
+           <div class="column columns-two-right">
+                <div id="page-zones__template-widgets__content-footercolumn2content1" class="widget content " data-widget-type="content" data-uniqueid="page-zones__template-widgets__content-footercolumn2content1">
+      <div class="bk-content-text js-text-content wysihtml-editor-content">
+                <h3>Connect:</h3>
+                </div>
+
+    
+</div>
+
+
+                <div id="page-zones__template-widgets__profile-footercolumn2socialicons" class="widget profile " data-widget-type="profile" data-uniqueid="page-zones__template-widgets__profile-footercolumn2socialicons">
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+<div class='bk-profile-socialicons'>
+    <!-- linkedIn -->
+              
+    <!-- Twitter -->
+        <span class="bk-socialicons-twitter">
+      <a href="https://twitter.com/www.twitter.com/songaweek2016" target="_blank">Twitter</a>
+    </span>
+    
+    <!-- Facebook -->
+        <span class="bk-socialicons-facebook">
+      <a href="http://www.facebook/com/groups/songaweek2016" target="_blank">Facebook</a>
+    </span>
+    
+    <!-- Rss -->
+              
+      <!-- Google+ -->
+        <span class="bk-socialicons-googleplus">
+      <a href="https://plus.google.com/114980795991483587673" target="_blank">Google+</a>
+    </span>
+    
+    <!-- youtube -->
+        <span class="bk-socialicons-youtube">
+      <a href="https://www.youtube.com/channel/UC8-7e8KrvnxNsuEPuJpRJvw" target="_blank">Youtube</a>
+    </span>
+    
+    
+</div>
+
+</div>
+
+
+           </div>
+        </div>
+    </div>
+</footer>
+
     <script src="../../js/jquery.min.js"></script>
     <script src="../../js/bootstrap.min.js"></script>
     <script src="../../js/scripts.js"></script>

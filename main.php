767f939d1b37fae0489440ab6d4f81c86866f9d8
Daniel Ault <dault@memphis.edu>
2016-02-21 22:57:43 +0000

    removed merge stuff from main.php

--- main.php
+++ main.php
@@ -7,11 +7,6 @@ echo "<br>\r\n";
 echo create_link("Submissions", "/songs/");
 echo "<br>\r\n";
 
-<<<<<<< HEAD
-=======
-
->>>>>> new_gui
-?>
 
 <a href="/" class="btn btn-info" role="button">Link Button</a>
 <button type="button" class="btn btn-info"><a href="/">Button</a></button>
